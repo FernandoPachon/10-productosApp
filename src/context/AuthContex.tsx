3962826575369db154ba88c5be3e925b390ed4db
Fernando Pachon <josep3194@gmail.com>
2022-12-22 21:00:06 -0500

    feat:adding reducer

--- src/context/AuthContex.tsx
+++ src/context/AuthContex.tsx
@@ -1,21 +1,37 @@
 import { createContext, useReducer } from "react";
 import { Usuario } from "../interfaces/appInterfaces";
+import { AuthState, authReducer } from './AuthReducer';
 
 type AuthContextProps={
     errorMessage?: string;
     token:string |null;
     user:Usuario |null;
-    status:'checking'|'authenticated'|'not-authenticated';
+    status:'checking'|'authenticated'|'not-authenticate';
     singUp:()=>void;
     singIn:()=>void;
     logOut:()=>void;
     removeError:()=>void;
 }
-export const AuthContext=createContext({}as AuthContextProps)
+const authInitialState:AuthState={
+    status:'checking',
+    token:null,
+    user:null,
+    errorMessage:''
+}
+export const AuthContext=createContext({}as AuthContextProps);
 export const AuthProvider=({children}:any)=>{
+    const [state, dispatch] = useReducer(authReducer,authInitialState);
+    const  singUp=()=>{};
+    const  singIn=()=>{};
+    const logOut=()=>{};
+    const  removeError=()=>{};
     return(
         <AuthContext.Provider value={{
-
+            ...state,
+            singIn,
+            singUp,
+            logOut,
+            removeError
         }}>
             {children}
         </AuthContext.Provider>
